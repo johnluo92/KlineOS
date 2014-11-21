fc9605f5d2885a9bd38d82c279af53cf7c943068
Barak Michaely <barakm18@gmail.com>
2014-11-21 16:52:21 -0500

    small changes

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Values in Microseconds
+		final int simulationTime; // 
+		final int quantum; //
+		final int contextSwitchTime;
+		final int avgProcessLength;
+		final int avgTimeBetweenProcesses;
+		final int avgIOInterruptLength;
+		
+		// Decimal representing percentage
+		final float ioBoundPercentage;
+		
+		// Parse Input File
+		
+		
 	}
 
 }
